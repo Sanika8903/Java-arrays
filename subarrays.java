938fdc0e2ec1d4e8dffaf866c1ec3cebe56cc657
Sanika <sanikajoshi8903@gmail.com>
2023-04-27 20:52:21 +0530

    printing all possible subarrays in an array

---
+++ subarrays.java
@@ -0,0 +1,29 @@
+package array;
+import java.util.*;
+
+public class subarrays {
+
+    public static void calculate(int numbers[],int n){
+        for (int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print("(");
+                for(int k=i;k<=j;k++){
+                    System.out.print(numbers[k] + " ");
+                }
+                System.out.print(") ");
+            }
+            System.out.println();
+        }
+    }
+
+   public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of the array: ");
+    int n = sc.nextInt();
+    int numbers [] = new int[n];
+    for (int i=0;i<n;i++){
+        numbers[i]=sc.nextInt();
+    }
+    calculate(numbers, n);
+   }
+}
